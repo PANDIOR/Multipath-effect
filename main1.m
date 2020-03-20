d1316ffdb75382dd42d9507f3657dec43a5254f1
panda <diaochenhao@163.com>
2020-03-21 01:11:31 +0800

    the last update

--- main1.m
+++ main1.m
@@ -1,30 +1,36 @@
 clear;
 clc;
-TimeStart=-pi;%起始时间
-dt=0.01;%采样率
-TimeEnd=10*pi;%终止时间
-t=TimeStart:dt:TimeEnd;
-
-distance1=500;%第一个障碍物多出的距离为500m
-distance2=1500;%第二个障碍物多出的距离为1500m
+%常数
 v_sound=340;%声音的传播速度340m/s
 v_wave=3*10^8;%电磁波传播速度
 
-alpha1=0.8;%第一条信道衰减系数
-alpha2=0.3;%第二条信道衰减系数
-alpha3=0.5;%第三条信道衰减系数
 
+%-----------------------参数设置区域--------------------------%
+distance1=500;%第一个障碍物多出的距离   单位：m
+distance2=1500;%第二个障碍物多出的距离  单位：m
+freq=10000;%激励正弦信号的频率   单位：Hz
+alpha1=0.8;%第一条信道衰减系数
+alpha2=0.6;%第二条信道衰减系数
+alpha3=0.7;%第三条信道衰减系数
+v=v_sound;%选择传播方式
+%------------------------------------------------------------%
+
+
+TimeStart=0;%起始时间
+dt=1/freq*0.001;%采样率
+TimeEnd=5/freq;%终止时间
+t=TimeStart:dt:TimeEnd;%进行采样
 %激励函数：sin(t)为标准正弦函数
 figure('name','多径效应基本模型');
-plot(t,sin(t));
+plot(t,sin(2*pi*freq*t));
 hold on;
-f=alpha1*sin(t)+alpha2*sin(t-distance1/v_sound)+alpha3*sin(t-distance2/v_sound);%响应表达式
+f=alpha1*sin(2*pi*freq*t)+alpha2*sin(2*pi*freq*(t-distance1/v))+alpha3*sin(2*pi*freq*(t-distance2/v));%响应表达式
+f=awgn(f,35);%加入高斯白噪声
 plot(t,f);
-
-xlim([0, 30]);
-ylim([-1.00 1.00]);
+xlim([0, 5/freq]);%只设定x轴的绘制范围
+ylim([-2, 2]);%只设定y轴的绘制范围
 xlabel('时间');
 ylabel('幅度');
 grid on;
-title('激励为sin(t)的多径模型');
+title('激励为正弦函数的多径模型');
 legend('发送信号','接收信号');
--- main2.m
+++ main2.m
@@ -1,23 +1,28 @@
 clear;
 clc;
 [y,Fs] = audioread('handel.wav');%读取音乐
-
 info = audioinfo('handel.wav');%获取音乐信息
 t = 0:seconds(1/Fs):seconds(info.Duration);
 t = t(1:end-1);
-distance1=500;%第一个障碍物多出的距离为5KM
-distance2=1500;%第二个障碍物多出的距离为20KM
-v_sound=340;%声音的传播速度340m/s
+
+
+%-----------------------参数设置区域--------------------------%
+distance1=500;%第一个障碍物多出的距离   单位：m
+distance2=1500;%第二个障碍物多出的距离  单位：m
+
 alpha1=0.2;%第一条信道衰减系数
 alpha2=0.3;%第二条信道衰减系数
 alpha3=0.5;%第三条信道衰减系数
+%------------------------------------------------------------%
+
+
+v_sound=340;%声音的传播速度340m/s
 figure('position',[200,100,1000,700]);
 subplot(3,1,1);
 plot(t,y);
 title('音频激励信号');
 xlabel('时间');
 ylabel('幅度');
-
 f1=y;
 f2=[zeros(floor(Fs*distance1/v_sound),1);y];
 f3=[zeros(floor(Fs*distance2/v_sound),1);y];
@@ -26,7 +31,7 @@ f=zeros(maxsize,1);
 f(1:size(f1,1))=alpha1*f1+f(1:size(f1,1));
 f(1:size(f2,1))=alpha2*f2+f(1:size(f2,1));
 f(1:size(f3,1))=alpha3*f3+f(1:size(f3,1));
-
+f=awgn(f,70);%加入高斯白噪声（大家可以试试吧70调成10，体验一下高噪声 =。= ）
 subplot(3,1,2);
 x=1:maxsize;
 plot(x/Fs,f,'-r');
@@ -34,11 +39,8 @@ xlim([-0.5, 14]);
 title('音频接收信号');
 xlabel('时间');
 ylabel('幅度');
-
-
 subplot(3,1,3);
 xx=1:info.TotalSamples;
-
 plot(xx/Fs,y);
 hold on;
 plot(x/Fs,f);
@@ -46,8 +48,7 @@ title('信号重叠对比图');
 xlabel('时间');
 ylabel('幅度');
 xlim([-0.5, 14]);
-
-sound(y,Fs);%播放原始音乐
+sound(y,Fs);%播放原始音乐（不想要听的话可以在前面加一个‘%’注释掉）
 pause(9);%等待9秒使音乐放完
 sound(f,Fs);%播放变换过音乐
 
